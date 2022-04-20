6a28cb4b09e0c850614f7869913b32ad26246ce7
unknown <darciodp2@gmail.com>
2022-04-19 21:49:12 -0300

    Importando e manipulando JSON

---
+++ Cinema.js
@@ -0,0 +1,6 @@
+var catalogo = require("./catalogo.json")
+console.log(catalogo);
+var catalogoString = JSON.stringify(catalogo);
+console.log(catalogoString)
+var catalogoObj = JSON.parse(catalogoString);
+console.log(catalogoObj)
