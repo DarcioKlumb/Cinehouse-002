757f339f1ca7f1217161c3f7e05e40d1f9df2e1a
DarcioKlumb <darciodp2@gmail.com>
2022-04-27 21:17:56 -0300

    Criado função listarFilmesEmCartaz

--- Cinema.js
+++ Cinema.js
@@ -1,13 +1,38 @@
-var catalogo = require("./catalogo.json")
-//console.log(catalogo);
+var catalogo = require("./catalogo.json");
+// console.log(catalogo);
 var catalogoString = JSON.stringify(catalogo);
-//console.log(catalogoString)
+// console.log(catalogoString);
 var catalogoObj = JSON.parse(catalogoString);
-//console.log(catalogoObj)
+// console.log(catalogoObj);
 
-function listarTodosOsFilmes (filmes){
-    for(let i= 0; i < filmes.length; i++) {
-        console.log(filmes[i].titulo)
-    }
+function listarTodosOsFilmes(filmes) {
+  for (let i = 0; i < filmes.length; i++) {
+    console.log(filmes[i].titulo);
+  }
 }
-listarTodosOsFilmes(catalogoObj.data);
+// listarTodosOsFilmes(catalogoObj.data);
+
+// const listarFilmesEmCartaz = (filmes) =>
+//   filmes.filter((filme) => filme.emCartaz);
+
+function listarFilmesEmCartaz(filmes) {
+  return filmes.filter((filme) => filme.emCartaz);
+}
+// console.log(listarFilmesEmCartaz(catalogoObj.data));
+
+function alterarStatusEmCartaz(id, filmes) {
+  const idxFilme = filmes.findIndex((filme) => {
+    if (filme.codigo === id) {
+      return true;
+    }
+  });
+  if (idxFilme >= 0) {
+    filmes[idxFilme].emCartaz = !filmes[idxFilme].emCartaz;
+    return true;
+  } else {
+    return false;
+  }
+}
+
+// console.log(alterarStatusEmCartaz(1, catalogoObj.data));
+console.log(catalogoObj.data);
